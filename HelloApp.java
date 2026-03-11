63b5c3736ae24afead4525f45d3bb6beb8362b3f
DishaunJose <dishaun.jose07@gmail.com>
2026-03-11 10:16:43 +0530

    Coded UC1: Display Hello World in console

---
+++ HelloApp.java
@@ -0,0 +1,13 @@
+/**
+ * HelloApp.java - A simple Java application that displays "Hello World"
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
